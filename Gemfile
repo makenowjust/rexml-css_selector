03a43992089fe6c3d0ab93b7a8240d2ac0c3b6fc
TSUYUSATO Kitsune <make.just.on@gmail.com>
2024-05-19 17:58:43 +0900

    bundle new rexml-css-selector

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in rexml-css-selector.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "minitest", "~> 5.16"
---
+++ lib/rexml/css/selector.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "selector/version"
+
+module Rexml
+  module Css
+    module Selector
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
