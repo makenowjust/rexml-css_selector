ed51f0bc6d195aa3cf2bf21da83120277f2a4cbb
TSUYUSATO Kitsune <make.just.on@gmail.com>
2024-05-29 17:55:02 +0900

    Update prism example

--- example/prism.rb
+++ example/prism.rb
@@ -8,6 +8,6 @@ program = Prism.parse(File.read(__FILE__)).value
 doc = REXML::CSSSelector::Adapters::PrismAdapter::PrismDOM.new(program)
 adapter = REXML::CSSSelector::Adapters::PrismAdapter::INSTANCE
 
-REXML::CSSSelector.each_select(doc, 'call[name="require"] > arguments:first-child > string', adapter:) do |element|
+REXML::CSSSelector.each_select(doc, 'call[name="require"] > arguments > string:first-child', adapter:) do |element|
   p element.node.content
 end
